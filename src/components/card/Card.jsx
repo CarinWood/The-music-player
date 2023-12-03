d266dc0289a546c1e0b79015e52c48362182dde9
CarinWood <carin.wood.85@gmail.com>
2023-12-03 13:42:36 +0100

    update

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,17 +1,19 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import './card.css'
 import { FaCirclePlay, FaCirclePause } from "react-icons/fa6";
 
 export const Card = ({id, img, title, artist, sound, setPlaySong, playSong}) => {
-
+    const [playBtn, setPlayBtn] = useState(true)
     const musicRef = useRef(new Audio(sound));
 
     const clickOnPlay = (id) => {
+        setPlayBtn(false)
         setPlaySong(id);
         musicRef.current.play();
     }
 
     const clickOnPause = () => {
+        setPlayBtn(true)
         musicRef.current.pause();
     }
 
@@ -29,18 +31,21 @@ export const Card = ({id, img, title, artist, sound, setPlaySong, playSong}) => 
             <p className='artist'>{artist}</p>
             <p className='title'>{title}</p>
         </div>
+        {playBtn ?
         <div 
             onClick={()=>clickOnPlay(id)}
             className='play'
         >
         <FaCirclePlay/>
         </div>
+        :
         <div 
             onClick={()=>clickOnPause()}
             className='play'
         >
         <FaCirclePause/>
         </div>
+        }
     
     </div>
   )
--- src/components/controlButtons/ControlButtons.jsx
+++ src/components/controlButtons/ControlButtons.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './controlButtons.css'
 import { FaCirclePlay, FaCirclePause } from "react-icons/fa6";
-import { IoMdSkipForward, IoMdSkipRewind } from "react-icons/io";
+import { IoMdSkipForward } from "react-icons/io";
 
 
 export const ControlButtons = ({setPlaySong, playSong}) => {
